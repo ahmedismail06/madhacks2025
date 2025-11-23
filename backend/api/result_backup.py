d60448ffbba045353cf915f3e72ff94b28a35626
Soham Mukherjee <smukherjee39.wisc.edu>
2025-11-23 11:43:53 -0600

    updated

--- backend/api/result_backup.py
+++ backend/api/result_backup.py
@@ -3,7 +3,7 @@
 # =====================================
 
 from flask import Response, app
-from core import plot_route_svg
+from  plot_route  import plot_route_svg  
 import requests  # or however you access /route/result internally
 
 
@@ -11,7 +11,7 @@ import requests  # or however you access /route/result internally
 def plot_route_svg_route():
 
     # Get final route result (you may call the internal function instead)
-    route_json = requests.get("http://localhost:5000/route/result").json()
+    route_json = requests.get("http://localhost:8000/route/result").json()
 
     if route_json.get("event") != "final-path":
         return Response("No path to plot", status=400)
